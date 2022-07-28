bcf86519b5cf3540dca986dd97bffb4803f5e641
DuncanEdwards <dun_edwards@yahoo.com>
2022-07-28 17:47:25 +0100

    Initial version

---
+++ src/partner-page/PartnerPage.tsx
@@ -0,0 +1,11 @@
+import data from "./partner-page-data.json";
+
+export const PartnerPage = () => {
+  return (
+    <div>
+      <div>Header Section</div>
+      <div>Main Section</div>
+      <div>Footer Section</div>
+    </div>
+  );
+};
